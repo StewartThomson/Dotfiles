7663d43ce12f3fe19bd18adf254d3a9895942ddb
Stewart Thomson <stewartthomson3@gmail.com>
2020-03-08 19:49:09 -0400

    changed fishfile

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,11 @@
-set -gx PATH $HOME/.cargo/bin $PATH
-
-set -gx SPACEFISH_EXIT_CODE_SHOW true
-
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+set -gx PATH $HOME/.cargo/bin $PATH
+
+set -gx SPACEFISH_EXIT_CODE_SHOW true
+
+set -gx fish_greeting "glhf"
